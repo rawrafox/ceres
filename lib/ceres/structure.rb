dfce2ccc2205c6894b73aae3e5b63d68d182f936
Aurora <aurora@aventine.se>
2016-12-03 01:57:18 +0100

    Minor refactoring

--- lib/ceres/structure.rb
+++ lib/ceres/structure.rb
@@ -18,56 +18,6 @@ module Ceres
       end
     end
 
-    def self.define_attribute(name, kind, options)
-      # TODO: Remove this requirement by invalidating @_structure_attributes of all subclasses
-      @_structure_finalized ||= false
-
-      raise ArgumentError, "safe structure is already finalized" if @_structure_finalized
-
-      @_structure_attributes ||= {}
-
-      raise ArgumentError, "attribute already defined" if @_structure_attributes.key?(name)
-
-      attribute = {
-        kind: kind,
-        name: name,
-        class: self,
-        getter: name,
-        setter: "#{name}=".to_sym
-      }.merge(options)
-
-      @_structure_attributes[name] = attribute
-
-      define_method(attribute[:getter]) do
-        @_structure_values[name]
-      end
-
-      define_method(attribute[:setter]) do |value|
-        value.freeze if attribute[:freeze]
-
-        if attribute[:type] && !value.is_a?(attribute[:type])
-          raise ArgumentError, "attribute #{attribute[:name]} (#{value.inspect}) is not of type " \
-                               "#{attribute[:type].name}"
-        end
-
-        if attribute[:enum] && !attribute[:enum].include?(value)
-          raise ArgumentError, "attribute #{attribute[:name]} (#{value.inspect}) is not one of " \
-                               "#{attribute[:enum].map(&:inspect).join(', ')}"
-        end
-
-        if attribute[:element_type] && !value.all? { |x| x.is_a?(attribute[:element_type]) }
-          raise ArgumentError, "attribute #{attribute[:name]} (#{value.inspect}) has elements of " \
-                               "other type than #{attribute[:element_type]}"
-        end
-
-        @_structure_values[name] = value
-      end
-
-      private attribute[:setter]
-
-      attribute[:getter]
-    end
-
     def self.attribute(name, type: nil, enum: nil, default: nil, optional: false, freeze: true)
       options = {
         type: type,
@@ -174,6 +124,71 @@ module Ceres
       end
 
       include Comparable
+    end
+
+    private_class_method def self.define_attribute(name, kind, options)
+      # TODO: Remove this requirement by invalidating @_structure_attributes of all subclasses
+      @_structure_finalized ||= false
+
+      raise ArgumentError, "safe structure is already finalized" if @_structure_finalized
+
+      @_structure_attributes ||= {}
+
+      raise ArgumentError, "attribute already defined" if @_structure_attributes.key?(name)
+
+      attribute = {
+        kind: kind,
+        name: name,
+        class: self,
+        getter: name,
+        setter: "#{name}=".to_sym,
+        public_getter: true,
+        public_setter: false
+      }.merge(options)
+
+      @_structure_attributes[name] = attribute
+
+      define_getter(attribute)
+      define_setter(attribute)
+
+      attribute[:getter]
+    end
+
+    private_class_method def self.define_getter(attribute)
+      name = attribute[:name]
+
+      define_method(attribute[:getter]) do
+        @_structure_values.fetch(name)
+      end
+
+      private attribute[:getter] unless attribute[:public_getter]
+    end
+
+    private_class_method def self.define_setter(attribute)
+      name = attribute[:name]
+
+      define_method(attribute[:setter]) do |value|
+        value.freeze if attribute[:freeze]
+
+        if attribute[:type] && !value.is_a?(attribute[:type])
+          raise ArgumentError, "attribute #{attribute[:name]} (#{value.inspect}) is not of type " \
+                               "#{attribute[:type].name}"
+        end
+
+        if attribute[:enum] && !attribute[:enum].include?(value)
+          raise ArgumentError, "attribute #{attribute[:name]} (#{value.inspect}) is not one of " \
+                               "#{attribute[:enum].map(&:inspect).join(', ')}"
+        end
+
+        if attribute[:element_type] && !value.all? { |x| x.is_a?(attribute[:element_type]) }
+          raise ArgumentError, "attribute #{attribute[:name]} (#{value.inspect}) has elements of " \
+                               "other type than #{attribute[:element_type]}"
+        end
+
+        @_structure_values[name] = value
+      end
+
+      private attribute[:setter] unless attribute[:public_setter]
     end
   end
 end
