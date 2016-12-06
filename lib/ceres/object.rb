645e8c77b685dc8a25e5f6d677172f7008ef1aa3
Aurora <aurora@aventine.se>
2016-12-06 01:32:34 +0100

    Add direct_descendants method

--- lib/ceres/object.rb
+++ lib/ceres/object.rb
@@ -6,9 +6,19 @@ module Ceres
       ObjectSpace.each_object(klass.singleton_class).select { |k| k != klass }
     end
 
+    def self.direct_descendants_of(klass)
+      descendants = self.descendants_of(klass)
+
+      descendants.reject { |k| descendants.any? { |c| c > k } }
+    end
+
     refine ::Object do
       def descendants
         Ceres::Object.descendants_of(self)
+      end
+
+      def direct_descendants
+        Ceres::Object.direct_descendants_of(self)
       end
     end
   end
--- spec/object_spec.rb
+++ spec/object_spec.rb
@@ -7,29 +7,40 @@ require "spec_helper"
 require "ceres/environment"
 
 RSpec.describe Ceres::Object do
-  it "calculates descendants_of" do
-    a = Class.new
-    b = Class.new(a)
-    c = Class.new(b)
-    d = Class.new(b)
+  before do
+    @a = Class.new
+    @b = Class.new(@a)
+    @c = Class.new(@b)
+    @d = Class.new(@b)
+  end
 
-    expect(Ceres::Object.descendants_of(a).to_set).to eq([b, c, d].to_set)
-    expect(Ceres::Object.descendants_of(b).to_set).to eq([c, d].to_set)
-    expect(Ceres::Object.descendants_of(c).to_set).to eq([].to_set)
-    expect(Ceres::Object.descendants_of(d).to_set).to eq([].to_set)
+  it "calculates descendants_of" do
+    expect(Ceres::Object.descendants_of(@a).to_set).to eq([@b, @c, @d].to_set)
+    expect(Ceres::Object.descendants_of(@b).to_set).to eq([@c, @d].to_set)
+    expect(Ceres::Object.descendants_of(@c).to_set).to eq([].to_set)
+    expect(Ceres::Object.descendants_of(@d).to_set).to eq([].to_set)
+  end
+
+  it "calculates direct_descendants_of" do
+    expect(Ceres::Object.direct_descendants_of(@a).to_set).to eq([@b].to_set)
+    expect(Ceres::Object.direct_descendants_of(@b).to_set).to eq([@c, @d].to_set)
+    expect(Ceres::Object.direct_descendants_of(@c).to_set).to eq([].to_set)
+    expect(Ceres::Object.direct_descendants_of(@d).to_set).to eq([].to_set)
   end
 
   using Ceres::Object
 
   it "calculates descendants", refinements: true do
-    a = Class.new
-    b = Class.new(a)
-    c = Class.new(b)
-    d = Class.new(b)
+    expect(@a.descendants.to_set).to eq([@b, @c, @d].to_set)
+    expect(@b.descendants.to_set).to eq([@c, @d].to_set)
+    expect(@c.descendants.to_set).to eq([].to_set)
+    expect(@d.descendants.to_set).to eq([].to_set)
+  end
 
-    expect(a.descendants.to_set).to eq([b, c, d].to_set)
-    expect(b.descendants.to_set).to eq([c, d].to_set)
-    expect(c.descendants.to_set).to eq([].to_set)
-    expect(d.descendants.to_set).to eq([].to_set)
+  it "calculates direct_descendants", refinements: true do
+    expect(@a.direct_descendants.to_set).to eq([@b].to_set)
+    expect(@b.direct_descendants.to_set).to eq([@c, @d].to_set)
+    expect(@c.direct_descendants.to_set).to eq([].to_set)
+    expect(@d.direct_descendants.to_set).to eq([].to_set)
   end
 end
